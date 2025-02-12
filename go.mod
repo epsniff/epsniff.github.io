df9661d08bbe023c20befe1d3a50eb86fc97f24d
epsniff <epsniff@gmail.com>
2025-02-12 09:55:20 -0800

    using go-app to build the initial page

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/epsniff/epsniff.github.io
+
+go 1.23.2
+
+require github.com/maxence-charriere/go-app/v10 v10.0.9
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	"github.com/maxence-charriere/go-app/v10/pkg/app"
+)
+
+/*
+# Build app.wasm:
+GOARCH=wasm GOOS=js go build -o /test-app/web/app.wasm
+
+# Build and generate static website:
+go build
+./hello
+*/
+
+// hello is a component that displays a simple "Hello World!". A component is a
+// customizable, independent, and reusable UI element. It is created by
+// embedding app.Compo into a struct.
+type hello struct {
+	app.Compo
+}
+
+// The Render method is where the component appearance is defined. Here, a
+// "Hello World!" is displayed as a heading.
+func (h *hello) Render() app.UI {
+	return app.H1().Text("Hello World!")
+}
+
+func main() {
+	app.Route("/", func() app.Composer { return &hello{} })
+	app.Route("/hello", func() app.Composer { return &hello{} })
+	app.RunWhenOnBrowser()
+
+	err := app.GenerateStaticWebsite(".", &app.Handler{
+		Name:        "Hello",
+		Description: "An Hello World! example",
+		Resources:   app.GitHubPages("epsniff/epsniff.github.io"),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
